c4d8e2499d250bc39619c3c77d96f549bb1f7b65
User <user@example.com>
2025-10-03 14:36:56 +0000

    Bump version to 1.2.0

--- custom_components/denon_avr_3805/const.py
+++ custom_components/denon_avr_3805/const.py
@@ -3,7 +3,7 @@
 NAME = "Denon AVR-3805"
 DOMAIN = "denon_avr_3805"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "1.1.3"
+VERSION = "1.2.0"
 MANUFACTURER = "Denon"
 
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
