cf2b865873abacba9fb09642f2f54ce3267e6ac3
User <user@example.com>
2025-10-03 18:56:58 +0000

    Update VERSION constant to 1.8.4 in const.py
    - Synchronized VERSION constant with manifest.json version
    - Ensures consistent version reporting in logs
    - No functional impact on device information display

--- custom_components/denon_avr_3805/const.py
+++ custom_components/denon_avr_3805/const.py
@@ -3,7 +3,7 @@
 NAME = "Denon AVR-3805"
 DOMAIN = "denon_avr_3805"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "1.8.2"
+VERSION = "1.8.4"
 MANUFACTURER = "Denon"
 
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
